59feca1699f4746d6ee4607ea2b282393ec41136
Yaroslav Serediuk <seredyukua@gmail.com>
2020-06-23 23:05:24 +0300

    Updates

---
+++ precache-manifest.bd9c3d7568e65b8dad5b2e11181ecbd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30400735a701ac68eb1c3a965007086",
+    "url": "/expenses-manager/index.html"
+  },
+  {
+    "revision": "7d7ec7f95744b677cdf3",
+    "url": "/expenses-manager/static/js/2.eeb44283.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/expenses-manager/static/js/2.eeb44283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b64ddd37584eb864b55",
+    "url": "/expenses-manager/static/js/main.407bdd0c.chunk.js"
+  },
+  {
+    "revision": "32809a24deaa760c4834",
+    "url": "/expenses-manager/static/js/runtime-main.fdc1d288.js"
+  }
+]);
